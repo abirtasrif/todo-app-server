59d3e38d549ff6c4c0a92039740b6f38f9747167
Abir Tasrif Anto <abirtasrif@gmail.com>
2022-12-24 00:29:16 +0600

    server complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const midddlewares = jsonServer.defaults();
+const port = process.env.port || 5000;
+
+server.use(midddlewares);
+server.use(router);
+server.listen(port);
